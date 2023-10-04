3a95aca918ca147bb67286191e6dfb395bcd4ad6
ntr0pie <tripathi.shreeshv@gmail.com>
2023-10-04 12:44:26 -0400

    Get num lasers

--- lab5/src/lasers.py
+++ lab5/src/lasers.py
@@ -1,9 +1,10 @@
 import sys 
 import os 
+import re 
 
 DEBUG = False
 
-def read_file(file_name: str) -> list[list]:
+def read_file(file_name: str) -> list[list[int]]:
     grid = []
     path = '../data'
     file_path = os.path.join(path, file_name)
@@ -23,7 +24,7 @@ def print_grid(grid: list[list]) -> None:
             print(cell, end=' ')
         print()
 
-def get_grid() -> None: 
+def get_grid() -> list[list[int]]: 
     if len(sys.argv) == 2:
         grid = read_file(sys.argv[1])
     elif len(sys.argv) == 3:
@@ -36,11 +37,30 @@ def get_grid() -> None:
         print("Enter a valid file name")
 
     return grid
+def is_valid_value(value: int, max_value: int) -> bool:
+    if re.fullmatch(r'^[1-9][0-9]*$', value):
+        if int(value) <= max_value:
+            return True
+        else:
+            print("Too many lasers to place!")
+    else:
+        print("Enter a valid positive integer.")
+
+def get_num_lasers(max_num: int) -> int: 
+    num_lasers = -1
+    while True:
+        num_lasers = input("Enter number of lasers: ").strip()
+        if is_valid_value(num_lasers, max_num):
+            num_lasers = int(num_lasers)            
+            break
+    return num_lasers
 
 def main():
     grid = get_grid()
     print_grid(grid)
-    
+    max_num_lasers = len(grid) * len(grid[0]) - 4
+    num_lasers = get_num_lasers(max_num_lasers)
+
 if __name__ == '__main__':
     main()
 
