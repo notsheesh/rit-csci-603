26c522ea29dbe62ae5d6b7d9d51844a5ddb7c3c1
ntr0pie <tripathi.shreeshv@gmail.com>
2023-10-04 17:47:46 -0400

    Make grid dict

--- lab5/src/lasers.py
+++ lab5/src/lasers.py
@@ -55,11 +55,90 @@ def get_num_lasers(max_num: int) -> int:
             break
     return num_lasers
 
+def is_valid_index(grid: list[list[int]], index) -> bool: 
+    row_index = index[0]
+    col_index = index[1]
+    if row_index in range(0, len(grid)):
+        if col_index in range(0, len(grid[0])):
+            return True
+    return False
+
+def is_valid_placement(grid, indices):
+    for index in indices:
+        if not is_valid_index(grid, index):
+            return False
+    return True
+
+def get_indices(direction: str, index: tuple[int]):
+    row_index = index[0]
+    col_index = index[1]
+
+    U = (row_index-1, col_index) # ^ 
+    D = (row_index+1, col_index) # v
+    L = (row_index, col_index-1) # <
+    R = (row_index, col_index+1) # >
+
+    if direction == 'N':
+        return (L, U, R) # < ^ >
+    if direction == 'S':
+        return (L, D, R) # < v > 
+    if direction == 'W':
+        return (D, L, U) # v < ^
+    if direction == 'E':
+        return (D, R, U) # v > ^
+
+
+def is_valid_direction(direction, grid, index):
+    indices = get_indices(direction, index)
+    if is_valid_placement(grid, indices):
+        return True
+    return False
+
+# delete comments here
+def get_direction_sum(direction, grid: list[list[int]],  index: tuple[int]):
+    indices = get_indices(direction, index)
+    sum = 0
+
+    # print("index: {} | direction: {}".format(index, direction))
+    for idx in indices: 
+        # print("Index: {} | Value: {}".format(idx, grid[idx[0]][idx[1]]))
+        sum += grid[idx[0]][idx[1]]
+    # print("sum: {}".format(sum))
+    # print("##################")
+    return sum
+
+def make_index_dict(grid, index):
+    index_dict = {
+            'max_dir': '',
+            'max_sum': -1
+        }
+
+    for direction in ['N', 'S', 'W', 'E']:
+        if is_valid_direction(direction, grid, index):
+            direction_sum = get_direction_sum(direction, grid, index)
+            if direction_sum > index_dict['max_sum']:
+                index_dict['max_dir'] = direction
+                index_dict['max_sum'] = direction_sum
+    
+    return index_dict
+
+def make_grid_dict(grid: list[list[int]]) -> dict:
+    grid_dict = {}
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            index = (i, j)
+            index_dict = make_index_dict(grid, index)
+            if index_dict['max_sum'] != -1:
+                grid_dict[index] = index_dict
+    return grid_dict
+
 def main():
     grid = get_grid()
     print_grid(grid)
     max_num_lasers = len(grid) * len(grid[0]) - 4
     num_lasers = get_num_lasers(max_num_lasers)
+    grid_dict = make_grid_dict(grid)
+    # sum_dict = make_sum_dict(grid_dict)
 
 if __name__ == '__main__':
     main()
