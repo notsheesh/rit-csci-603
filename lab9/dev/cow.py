bfecbf8716b91bb8b4d22bb407768c6958978da0
ntr0pie <tripathi.shreeshv@gmail.com>
2023-11-29 20:59:16 -0500

    Add version 2

--- lab9/dev/cow.py
+++ lab9/dev/cow.py
@@ -1,9 +1,27 @@
 class Cow:
+    """
+    Represents a cow with a unique identifier, location, orientation, and type.
+    """
+
     def __init__(self, id, loc):
+        """
+        Initializes a new Cow instance.
+
+        :param id: A unique identifier for the cow.
+        :type id: str
+        :param loc: A tuple representing the (x, y) coordinates of the cow's location.
+        :type loc: tuple
+        """
         self.id = id
         self.loc = loc
         self.r = 0
         self.type = "cow"
 
     def __str__(self):
+        """
+        Returns a string representation of the cow based on its unique identifier.
+
+        :return: A string representing the cow's unique identifier.
+        :rtype: str
+        """
         return self.id
